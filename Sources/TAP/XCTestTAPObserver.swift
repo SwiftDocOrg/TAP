da697002ab728e73b42f008b0093fd16e7f6bbfc
Mattt <mattt@me.com>
2020-11-29 07:06:02 -0800

    Add XCTestTAPObserver

---
+++ Sources/TAP/XCTestTAPObserver.swift
@@ -0,0 +1,37 @@
+#if canImport(XCTest)
+import XCTest
+
+public class XCTestTAPObserver: NSObject {
+    private var reporter: Reporter?
+}
+
+extension XCTestTAPObserver: XCTestObservation {
+    public func testSuiteWillStart(_ testSuite: XCTestSuite) {
+        reporter = Reporter(numberOfTests: testSuite.testCaseCount)
+    }
+
+    public func testCaseDidFinish(_ testCase: XCTestCase) {
+        reporter?.report(.success(testCase.qualifiedName, directive: testCase.directive, metadata: nil))
+    }
+
+    public func testCase(_ testCase: XCTestCase, didFailWithDescription description: String, inFile filePath: String?, atLine lineNumber: Int) {
+        let metadata: [String: Any] = [
+            "description": description,
+            "file": filePath as Any,
+            "line": lineNumber
+        ].compactMapValues { $0 }
+
+        reporter?.report(.failure(testCase.qualifiedName, directive: testCase.directive, metadata: metadata))
+    }
+}
+
+fileprivate extension XCTestCase {
+    var qualifiedName: String {
+        "\(className).\(name)"
+    }
+
+    var directive: Directive? {
+        return testRun?.hasBeenSkipped == true ? .skip(explanation: nil) : nil
+    }
+}
+#endif
